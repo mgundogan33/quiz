55c1d8ea04bfd2b74902b06e4d9b5635746b74ca
mgundogan33 <gundogan.mehmet33@gmail.com>
2022-04-08 22:30:40 +0300

    3 bitti

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace Database\Seeders;
+use Illuminate\Support\Str;
+
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -14,6 +16,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::insert([
+            'name'=>'Mehmet Gündoğan',
+            'email'=>'gundogan.mehmet33@gmail.com',
+            'email_verified_at' => now(),
+            'type'=>'admin',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
+        \App\Models\User::factory(5)->create();
     }
 }
